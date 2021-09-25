e8c79d0c777cc0620394b48cffd9b6b304bf8db9
dvramaraju <ramarajudv21@gmail.com>
2021-09-25 21:46:52 +0530

    ADD: Sections for the TESLA

---
+++ src/components/Section.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+  downArrow,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        {description && <p>{description}</p>}
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        {downArrow && <DownArrow src="/images/down-arrow.svg"></DownArrow>}
+      </Buttons>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const Buttons = styled.div``;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: felx;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 10px;
+`;
+const RightButton = styled(LeftButton)`
+  background-color: #ffffff;
+  opacity: 0.65;
+  color: black;
+`;
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1s;
+`;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import Home from "./Home";
+import Section from "./Section";
+
+export { Header, Home, Section };
